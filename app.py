8a4575c1532d8579ecbee4349dbab3fb21a20d3c
billysnaith <47987932+billysnaith@users.noreply.github.com>
2020-06-07 17:10:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 birth_year = input('When were you born? ')
 birth_year = int(birth_year)
 age = 2020 - birth_year
-print(age)
-if(age)<25 
+if age < 25:
     print('You are too young to drink')
-    else
-    print('You are old!')
-    endif
+
     
     
     
